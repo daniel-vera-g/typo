70d8b5622c7563a106ca6103b1143cb71bd79652
danielVg <danielveragi@gmail.com>
2021-05-02 16:57:55 +0200

    Space :)

--- scripts/sync-word-list.sh
+++ scripts/sync-word-list.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Sync local vscode spell check and textidote word list
-jq  -r '.words[]' .vscode/cspell.json   > .github/mydict.txt
+jq  -r '.words[]' .vscode/cspell.json  > .github/mydict.txt
